bdd391ada1cd577e8e76ab9c9ae9b13d694ae05f
Chance Back <chancedback@gmail.com>
2023-04-21 17:48:15 -0500

    All buttons work right now

--- calls.py
+++ calls.py
@@ -4,25 +4,60 @@ from ip2geotools.databases.noncommercial import DbIpCity # used to get location 
 import public_ip as ip # used to get public ip address
 
 country = 'US'
-appid = '5c473fac0bf570ae74bd2aff29b8a8ed'
+appid = 'ffa78836e9836379d10c84b90c28728b'
 
-def get_weather_name(city, state, country=country, appid=appid):
-    res = requests.get(f'https://api.openweathermap.org/data/2.5/weather?q={city},{state},{country}&appid={appid}&units=imperial')
+def get_weather(city=None, state=None, country=country, appid=appid):
+    # check if city/state passed
+    if not city and not state:
+        location = get_user_location()
+        # request to get geo coords
+        geo_coords = get_geo_coords(location[0], location[1])
+        city_st_tuple = (location[0], location[1])
+    else:
+        city_st_tuple = (city, state)
+        geo_coords = get_geo_coords(city, state, country)
+        city_st_tuple = (city, state)
+
+    lat, long = geo_coords[0], geo_coords[1]
+
+    # request to get weather info
+    res = requests.get(f'https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={long}&appid={appid}&units=imperial')
     weather_info = res.json()
-    return weather_info
 
-def get_weather_location():
+    return weather_info, city_st_tuple
+
+def get_forecast(city=None, state=None, country=country, appid=appid):
+    # check if city/state passed
+    if not city and not state:
+        location = get_user_location()
+        # request to get geo coords
+        geo_coords = get_geo_coords(location[0], location[1])
+        city_st_tuple = (location[0], location[1])
+    else:
+        city_st_tuple = (city, state)
+        geo_coords = get_geo_coords(city, state, country)
+        city_st_tuple = (city, state)
+
+    lat, long = geo_coords[0], geo_coords[1]
+    # request to get geo coords
+    res = requests.get(f'http://api.openweathermap.org/data/2.5/forecast/daily?lat={lat}&lon={long}&appid={appid}&cnt=7&units=imperial')
+    forecast_info = res.json()
+    return forecast_info, city_st_tuple
+
+def get_geo_coords(city, state, country=country, appid=appid):
+    # request to get geo coords
+    geo_coords = requests.get(f'http://api.openweathermap.org/geo/1.0/direct?q={city},{state},{country}&limit=1&appid={appid}')
+    geo_coords = geo_coords.json()
+    lat = geo_coords[0]['lat']
+    long = geo_coords[0]['lon']
+
+    return (lat, long)
+
+def get_user_location():
     ip_add = ip.get() # gets public ip address
     res = DbIpCity.get(ip_add, api_key="free") # finds location based on public ip
-    weather_info = get_weather_name(res.city, res.region, res.country) # gets weather info from current location
-    return weather_info
-
-def get_weather_forecast(city, state, country=country, appid=appid):
-    res = requests.get(f'https://api.openweathermap.org/data/2.5/forecast?q={city},{state},{country}&appid={appid}&units=imperial')
-    weather_info = res.json()
-    return weather_info
-
+    return (res.city, res.region)
 ###########test############
 
 if __name__ == '__main__':
-    print(get_weather_forecast('Denver', 'CO'))
+    print(get_forecast())
--- weatherApp.py
+++ weatherApp.py
@@ -6,6 +6,8 @@ import yaspin # loading icon/animation
 
 from calls import *
 
+import time # for time conversions
+
 class WeatherApp:
     """
     Represents the app itself.
@@ -63,26 +65,26 @@ class WeatherApp:
             # Get city data via API
             # Navigate to city menu
             city_st_tuple = self.__get_city()
-            weather_info = self.__call_api(city_st_tuple)
+            weather_info, location = self.__call_api(city_st_tuple)
             self.__city_menu(weather_info, city_st_tuple)
 
         elif cmd == nav2: 
-            weather_info = self.__call_api()
-            self.__city_menu(weather_info)
+            weather_info, city_st_tuple = self.__call_api()
+            self.__city_menu(weather_info, city_st_tuple)
         else: return
 
-    def __city_menu(self, weather_info, city_st_tuple):
+    def __city_menu(self, weather_info, city_st_tuple=None):
         #List options:
         # Detailed View
         # Weekly Forecast
         # Add to Favorites
         # Back to Main Menu
-        self.__display_weather(weather_info)
+        self.__display_weather(weather_info, city_st_tuple)
 
         while True:
             nav1 = "Detailed View"
             nav2 = "Weekly Forecast"
-            nav3 = "Add to Favorite"
+            nav3 = "Add to Favorites"
             nav4 = "Back"
             
             questions = [
@@ -94,14 +96,14 @@ class WeatherApp:
             answers = inquirer.prompt(questions)
             cmd = answers['cmd']
 
-            if cmd == nav1: self.__display_weather(weather_info, detailed=True)
+            if cmd == nav1: self.__display_weather(weather_info, city_st_tuple, detailed=True)
             elif cmd == nav2: 
                 #print('Sorry this feature is currently unavailable through the free subscription of the weather API I\'m using.')
-                forecast_info = self.__call_api(city_st_tuple, forecast=True)
+                forecast_info, location = self.__call_api(city_st_tuple, forecast=True)
                 self.__display_forecast(forecast_info) # or maybe just pass it the city/state
             elif cmd == nav3: 
-                self.__add_favorite(weather_info) # or just pass city/state as well..
-                print("Successfully added city to favorites list!")
+                self.__add_favorite(city_st_tuple) # or just pass city/state as well..
+                print(f"Successfully added {city_st_tuple} to Favorites List!")
             elif cmd == nav4: break
     
     def __favorites_menu(self):
@@ -128,10 +130,24 @@ class WeatherApp:
             cmd = answers['cmd']
 
             if cmd == nav1: self.__display_favorites()
-            elif cmd == nav2: self.__remove_city()
+            elif cmd == nav2:
+                self.__display_favorites()
+                index = input("What is the number associated with the city you would like removed? ")
+                index = int(index) - 1
+                self.__remove_favorite(index)
+                print(f"Successfully removed city from Favorites List!")
             elif cmd == nav3:
-                # Are you sure? Y/N question here 
-                self.__clear_favorites()
+                questions = [
+                inquirer.List('cmd',
+                                message="Are you sure? This action will permanently delete all cities in Favorites?",
+                                choices=['Yes', 'No'],
+                            ),
+                ]
+                answers = inquirer.prompt(questions)
+                cmd = answers['cmd']
+                if cmd == 'Yes':
+                    self.__clear_favorites()
+                print("Successfully Cleared Favorites List!")
             else: break
 
     def __get_city(self):
@@ -143,12 +159,14 @@ class WeatherApp:
         return (answers['city'], answers['state'])
         
     def __call_api(self, city_st_tuple=None, forecast=False):
-        if city_st_tuple == None: city_data = get_weather_location()
-        elif forecast == True: city_data = get_weather_forecast(city_st_tuple[0], city_st_tuple[1])
-        else: city_data = get_weather_name(city_st_tuple[0], city_st_tuple[1])
-        return city_data
+        if not city_st_tuple and not forecast: city_data, location = get_weather()
+        elif city_st_tuple and not forecast: city_data, location = get_weather(city_st_tuple[0], city_st_tuple[1])
+        elif not city_st_tuple and forecast: city_data, location = get_forecast()
+        elif city_st_tuple and forecast: city_data, location = get_forecast(city_st_tuple[0], city_st_tuple[1])
+        return city_data, location
 
-    def __display_weather(self, weather_info, detailed=False):
+    def __display_weather(self, weather_info, city_st_tuple, detailed=False):
+        print(city_st_tuple)
         print(f"Temp: {weather_info['main']['temp']}")
         print(f"Feels Like: {weather_info['main']['feels_like']}")
         print(f"{weather_info['weather'][0]['main']}: {weather_info['weather'][0]['description']}")
@@ -157,12 +175,22 @@ class WeatherApp:
             print("More details here")
 
     def __display_forecast(self, forecast_info):
-        print(forecast_info)
+        for date in forecast_info['list']:
+            day = time.ctime(date['dt'])
+            day = day[:10]
+            print(f"{day}:      {date['temp']['max']}  {date['temp']['min']}")
 
     def __add_favorite(self, city_st_tuple):
         if len(self._favorite_list) < 10:
             self._favorite_list.append(city_st_tuple)
         else: print("Sorry Favorites List is currently full")
+
+    def __remove_favorite(self, index):
+        city = self._favorite_list[index]
+        self._favorite_list.remove(city)
+
+    def __clear_favorites(self):
+        self._favorite_list.clear()
 
     def __display_favorites(self):
         for count, city in enumerate(self._favorite_list, 1):
